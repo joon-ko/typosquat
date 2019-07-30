0c3b13c86cd6ee70af60ccbf4db46a18ea0cef09
simpko <joonhok@mit.edu>
2019-07-30 11:58:55 -0700

    add function for domain name availability and price

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # typosquat
+
 detect typosquatted domain names 
 
 developed during godaddy intern week 2019 by joonho ko, alex yang, jeff chow, and jeana choi.
---
+++ domain.py
@@ -0,0 +1,28 @@
+import requests
+import whois
+
+
+def get_domain_information(domain_name):
+    """
+    Returns a tuple (availabile, valuation).
+    availabile: boolean, whether or not domain name is available
+    valuation: number, price of domain name. None if available is False.
+    """
+    endpoint = 'https://entourage.dev.aws.godaddy.com/domainsapi/v1/search/exact?q={}'.format(domain_name)
+    r = requests.get(endpoint)
+    rj = r.json()
+    available = rj['ExactMatchDomain']['IsAvailable']
+    valuation = rj['ExactMatchdomain']['Price'] if available else None
+    return (available, valuation)
+
+
+def get_domain_availability(domain_name):
+    """
+    Slightly janky way to get domain availability using a third party tool.
+    Return True if domain name is available, False otherwise.
+    """
+    try:
+        w = whois.whois(domain_name)
+    except whois.parser.PywhoisError: # domain name not found
+        return False
+    return not bool(w['registrar']) # if registrar field is None, it's not registered
