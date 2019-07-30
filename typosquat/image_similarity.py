0a33c67966cfb2500572350b254af3bbe22d4c1c
jefftienchow <jeffchow@mit.edu>
2019-07-30 16:17:39 -0700

    feat(similarity): added similarity calculator for images and their image files

---
+++ typosquat/image_similarity.py
@@ -0,0 +1,25 @@
+from PIL import Image
+import imagehash
+
+substitutions = {
+	"0": ["O"],
+	"c": ["o"],
+	"e": ["o"],
+	"l": ["I", "1"],
+	"m": ["rn", "n"],
+	"t": ["l"],
+	"u": ["v"],
+	"w": ["vv", "v"],
+}
+
+# gets similarity score of between letter substitutions given that their png file exists in text_images/{letter}.png
+def get_similarity_score(substitutions)
+	scores = {}
+	for char in substitutions:
+		hash = imagehash.average_hash(Image.open('text_images/{}.png'.format(char)))
+		for other_char in substitutions[char]:
+			otherhash = imagehash.average_hash(Image.open('text_images/{}.png'.format(other_char)))
+			print("score for ", char, " and ", other_char, "is ", abs(hash - otherhash))
+			scores[(char, other_char)] = abs(hash - otherhash)
+			scores[(other_char, char)] = abs(hash - otherhash)
+	return scores
