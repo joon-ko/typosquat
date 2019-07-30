a0999a8a3656980615c19cbe2a61fd2f522e0b03
jchoi614 <jeanachoi@gmail.com>
2019-07-30 16:32:54 -0700

    feat(image): created dynamic cropping of images

--- typosquat/text_to_image.py
+++ typosquat/text_to_image.py
@@ -8,9 +8,11 @@ class Font(Enum):
 	OPEN_SANS = "fonts/OpenSans.ttf"
 	VERDANA = "fonts/Verdana.ttf"
 
-def draw_text(text, font=Font.HELVETICA, font_size=14, img_filename='test'):
+def draw_text(text, font=Font.HELVETICA, font_size=100, img_filename='test'):
 	font = ImageFont.truetype(font.value, font_size)
-	im = Image.new('RGB', (200, font_size), color=(255, 255, 255))
+	ascent, descent = font.getmetrics()
+	(width, baseline), (offset_x, offset_y) = font.font.getsize(text)
+	im = Image.new('RGB', (width, ascent - offset_y + descent), color=(255, 255, 255))
 	draw = ImageDraw.Draw(im)
 	draw.text((0, 0), text, font=font, fill=(0, 0, 0, 255))
 	im.save("text_images/%s.png" % (img_filename), "PNG")
