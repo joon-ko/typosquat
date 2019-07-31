9a870142ae099f4f078cb9d5a1b6905056a8a189
Alexander Yang (Intern) <alexy@mit.edu>
2019-07-30 17:42:26 -0700

    added basic ajax/js stuff for formatting domain entries

--- typosquat/static/js/index.js
+++ typosquat/static/js/index.js
@@ -1,3 +1,62 @@
+function setEntry(entry, index, domainName) {
+    entry.setAttribute("id", "r1-" + String(index));
+    entry.setAttribute("class", "result results_links_deep highlight_d result--url-above-snippet");
+    entry.setAttribute("data-domain", domainName);
+    entry.setAttribute("data-hostname", domainName);
+    entry.setAttribute("data-nir", "1");
+
+    let resultBody = document.createElement('div');
+    resultBody.setAttribute("class", "result__body links_main links_deep");
+
+    let resultTitle = document.createElement('h2');
+    resultTitle.setAttribute("class", "result__title");
+
+    let resultA = document.createElement('a');
+    resultA.setAttribute("class", "result__a");
+    resultA.setAttribute("rel", "noopener");
+    resultA.setAttribute("href", domainName);
+    resultA.innerHTML = domainName;
+    resultTitle.appendChild(resultA);
+    resultBody.appendChild(resultTitle);
+
+    let resultExtras = document.createElement('div');
+    resultExtras.setAttribute("class", "result__extras js-result-extras");
+
+    let resultExtrasURL = document.createElement('div');
+    resultExtras.setAttribute("class", "result__extras__url");
+
+    // let resultIcon = document.createElement('span');
+    // resultIcon.setAttribute("class", "result__icon");
+
+    let resultHref = document.createElement('a');
+    resultHref.setAttribute("href", domainName);
+    resultHref.setAttribute("rel", "noopener");
+    resultHref.setAttribute("class", "result__url js-result-extras-url");
+
+    let resultURLDomain = document.createElement('span');
+    resultURLDomain.setAttribute("class", "result__url__comain");
+    resultURLDomain.innerHTML = domainName
+
+    let resultURLFull = document.createElement('span');
+    resultURLDomain.setAttribute("class", "result__url__full");
+
+    resultHref.appendChild(resultURLDomain);
+    resultHref.appendChild(resultURLFull);
+
+    resultExtrasURL.appendChild(resultHref);
+
+    resultExtras.appendChild(resultExtrasURL);
+
+    let resultSnippet = document.createElement('div');
+    resultSnippet.setAttribute("class", "result__snippet js-result-snippet");
+    resultSnippet.innerHTML = "This domain has not yet been bought."; // TODO: actually make this
+
+    resultBody.appendChild(resultExtras);
+    resultBody.appendChild(resultSnippet);
+
+    entry.appendChild(resultBody);
+}
+
 function jeff() {
     let domainName = document.getElementById('search_form_input_homepage').value;
     fetch('/result', {
@@ -5,11 +64,20 @@ function jeff() {
         body: domainName
     }).then(response => {
         response.json().then(data => {
-            let resultContainer = document.getElementById('search_form_homepage');
+            let resultContainer = document.getElementById('links');
             resultContainer.innerHTML = '';
-            let result = document.createElement('div');
-            result.innerHTML = data;
-            resultContainer.appendChild(result);
+            var eachDomainName = data;
+            // var eachDomainName = String(data).split(',');
+            var numDomains = eachDomainName.length;
+            for (var i = 0; i < numDomains; i++) {
+                let result = document.createElement('div');
+
+                setEntry(result, i, eachDomainName[i]);
+                resultContainer.appendChild(result);
+            }
+            // let result = document.createElement('div');
+            // result.innerHTML = data;
+            // resultContainer.appendChild(result);
         })
     });
 }
