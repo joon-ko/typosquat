430cf517f7ca12a6783b4282414c05a325d694d7
simpko <joonhok@mit.edu>
2019-07-31 05:00:49 -0700

    fix(run.sh): add export for weird fork() bug

--- run.sh
+++ run.sh
@@ -8,6 +8,9 @@ source venv/bin/activate
 # install dependencies
 pip install -r requirements.txt
 
+# required as a fix for an obscure fork() bug
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+
 # run app
 export FLASK_APP=typosquat/app.py
 flask run
