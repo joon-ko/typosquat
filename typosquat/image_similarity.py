f00b9baa346e4b5b87ca5625d0f5e09cb5640744
jefftienchow <jeffchow@mit.edu>
2019-07-30 17:28:31 -0700

    feat(similarity): normalized score

--- typosquat/image_similarity.py
+++ typosquat/image_similarity.py
@@ -22,4 +22,8 @@ def get_similarity_score(substitutions):
 			print("score for ", char, " and ", other_char, "is ", abs(hash - otherhash))
 			scores[(char, other_char)] = abs(hash - otherhash)
 			scores[(other_char, char)] = abs(hash - otherhash)
+	
+	max_score = max(scores.values())
+	for score in scores:
+		scores[score] = 1 - scores[score]/max_score
 	return scores
